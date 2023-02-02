ad755c1621c1892a50b8924e26c340cbe9450a1e
afiffasya <afiffasya18@gmail.com>
2023-02-02 10:47:21 +0700

    edit project

--- src/components/Newpro.js
+++ src/components/Newpro.js
@@ -1,8 +1,5 @@
-import Carousel from 'react-bootstrap/Carousel';
-import afsyatv1 from "../assets/img/afsyatv1.png";
-import afsyatv2 from "../assets/img/afsyatv2.png";
-import afsyatv3 from "../assets/img/afsyatv3.png";
-import afsyatv4 from "../assets/img/afsyatv4.png";
+import Card from 'react-bootstrap/Card';
+import vid from "../assets/img/vid.gif";
 import 'animate.css';
 
 export const Projectt = () => {
@@ -29,40 +26,9 @@ export const Projectt = () => {
         <div className='prj' id='projects'>
         <h1>Project</h1>
         <h4>Mini Project responsive website from dibimbing.id built using HTML/CSS and bootstrap</h4>
-      <Carousel responsive={responsive} infinite={true} className="prj-slider">
-        <Carousel.Item>
-          <img
-            src={afsyatv1}
-            alt=" "
-          />
-          <h5 className='detail'>homepage: Navbar-Alert-Carousel-Breadcrumbs
-          </h5>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            src={afsyatv2}
-            alt=" "
-          />
-          <h5 className='detail'>Form Login
-          </h5>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            src={afsyatv3}
-            alt=" "
-          />
-          <h5 className='detail'>Card: When clicking the card, a modal will appear with the full image of product
-          </h5>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            src={afsyatv4}
-            alt=" "
-          />
-          <h5 className='detail'>footer:
-          </h5>
-        </Carousel.Item>
-      </Carousel>
+        <Card responsive={responsive} infinite={true} className="prj-slider">
+      <Card.Img src={vid} />
+    </Card>
       </div>
     );
   }
